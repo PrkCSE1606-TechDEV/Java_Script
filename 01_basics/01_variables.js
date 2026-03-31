8830871cf131399c351d6612fe63aca6719bcb31
PrkCSE1606-TechDEV <prkcsc1606@gmail.com>
2026-03-31 18:58:27 +0000

    pushing all files

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,19 @@ const accountId = 144563
 let accountEmail = "priyanshu123@git.com"
 var accountPassword = "23456"
 accountCity = "Bhagalpur"
+let accountState;
 
 // accountId = 2 // not allowed 
 
+
+accountEmail = "prk@git.com"
+accountPassword = "2245"
+accountCity = "Patna"
+
 console.log(accountId); 
+
+/*
+Prefer not to use var. 
+Beacause of issue in block scope and functional scope.
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
