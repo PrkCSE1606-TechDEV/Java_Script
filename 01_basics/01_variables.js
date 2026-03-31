f043cc632274c5048b0551904c8e322ae70c3d47
PrkCSE1606-TechDEV <prkcsc1606@gmail.com>
2026-03-31 18:50:11 +0000

    added variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144563
+let accountEmail = "priyanshu123@git.com"
+var accountPassword = "23456"
+accountCity = "Bhagalpur"
+
+// accountId = 2 // not allowed 
+
+console.log(accountId); 
