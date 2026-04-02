a86298026f9cb0795854adf4c34abb7100382203
PrkCSE1606-TechDEV <prkcsc1606@gmail.com>
2026-04-02 16:53:17 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Priyanshu"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+let isLoggedIn = "Priyanshu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+/* 
+1 => true; 0 => false
+"Priyanshu" => true
+*/
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
