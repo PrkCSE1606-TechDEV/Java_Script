471e085f0d1987c4fe2f1c517f5d67d0be033ef6
PrkCSE1606-TechDEV <prkcsc1606@gmail.com>
2026-04-02 16:00:40 +0000

    First commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) We are using Nodejs, not browser.
+
+console.log(3
+    +
+    3) // Code readability should be high.
+
+console.log("Priynashu")
+
+
+let name = "Priyanshu"
+let age = 20
+let isLoggedIn = false
+let state; 
+
+/* number => 2 to power 53
+   bigint
+   string => ""
+   boolean => true/false
+   null => Standalone value
+   undefined => 
+   symbol => Unique
+
+
+   object
+*/
+console.log(typeof "Priyanshu"); //String DataType
+console.log(typeof age); //Number DataType
+console.log(typeof null); //Object DataType
+console.log(typeof undefined); //Undefined has itself as DataType
