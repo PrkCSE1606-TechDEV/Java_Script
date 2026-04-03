3f5549630eada368e33632f9a794a0c3ad15561d
PrkCSE1606-TechDEV <prkcsc1606@gmail.com>
2026-04-03 10:04:06 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,11 +32,40 @@ const myfunction = function(){
     
 } 
 
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
-console.log(typeof scoreValue);
-console.log(typeof myfunction);
-console.log(typeof heros);
-console.log(typeof anotherId);
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof scoreValue);
+// console.log(typeof myfunction);
+// console.log(typeof heros);
+// console.log(typeof anotherId);
 
 // Read about Typeof Operators from link: https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) => We get a copy of declared variable,
+// Heap (Non-Primitive) => It happens when a memory is defined under heap
+//                           We get the reference of original value.
+
+let myYouTubename = "Priyanshu_1606"
+
+let anothername = myYouTubename
+anothername = "Priyanshu_1610"
+
+console.log(myYouTubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "priyanshu@google.com" //=> All the properties of the Object like email, upi are accessed by putting a dot(.) before it.
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// Read MS Word file JavaScript in IMportant Pdf's Section.
